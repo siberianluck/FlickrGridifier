3591fbd356121f415b25e863552c5295ca9c5ea7
Siberianluck <bomason@gmail.com>
2011-02-13 14:19:03 -0700

    Login stuff

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+	const APIKEY = "";
+	const APISECRET = "";
+?>
--- gridifier.php
+++ gridifier.php
@@ -1,6 +1,13 @@
 <?php
+require_once config.php;
 
 //Authenticate flickr user
+//create login url
+$apiSig = APISECRET."api_key".APIKEY."permsread";
+$apiSigHash = md5($apiSig);
+$loginUrl = "http://flickr.com/services/auth/?api_key=".APIKEY."&perms=read&api_sig=$apiSigHash";
+
+echo "<a href=\"$loginUrl\">Login!</a>";
 
 //Get recent flickr pictures
 //flickr.photos.search needs user_id and per_page=20
