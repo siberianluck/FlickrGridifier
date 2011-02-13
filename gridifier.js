57ff78c124fe3b4502fba08784d44335e2318c0f
Siberianluck <bomason@gmail.com>
2011-02-13 13:12:44 -0700

    comment skeleton

---
+++ gridifier.js
@@ -0,0 +1,3 @@
+//attach events to images
+
+//Click will put an image in the next available gridspot.
---
+++ gridifier.php
@@ -0,0 +1,14 @@
+<?php
+
+//Authenticate flickr user
+
+//Get recent flickr pictures
+//flickr.photos.search needs user_id and per_page=20
+
+//Create blank grid
+
+//Button to do output
+
+//Send output code
+
+?>
