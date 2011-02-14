bc898f13b70b5afd1b642c1c652d16d4ed59bf86
Siberianluck <bomason@gmail.com>
2011-02-13 20:03:17 -0700

    add code select box, flickr logo for login

--- gridifier.php
+++ gridifier.php
@@ -29,8 +29,10 @@ if(isset($_REQUEST['frob'])){
 	$request = 'http://api.flickr.com/services/rest?method='.$apiMethod.'&api_key='.APIKEY.'&api_sig='.$apiSigHash.'&auth_token='.$token[0].'&user_id='.$userId[0].'&per_page=12';
 	$photosXml = new SimpleXMLElement(file_get_contents($request));
 ?>
-		<table>
+		<table style="border-spacing: 4px">
 <?php
+	//start code string
+	$codeString = "<table style=\"border-spacing: 4px\">";
 	//Display recent photos
 	$count = 0;
 	foreach($photosXml->photos->photo as $photo){
@@ -38,6 +40,7 @@ if(isset($_REQUEST['frob'])){
 		?>
 		<tr>
 		<?php
+		$codeString .= "<tr>";
 		}
 		$farmId = $photo['farm'];
 		$serverId = $photo['server'];
@@ -45,16 +48,24 @@ if(isset($_REQUEST['frob'])){
 		$photoSecret = $photo['secret'];
 		$photoUrl = "http://farm{$farmId}.static.flickr.com/{$serverId}/{$photoId}_{$photoSecret}_m.jpg";
 		$photoPageUrl = "http://www.flickr.com/photos/{$userId[0]}/{$photoId}";
-		echo '<td class="imgcell"><a href="'.$photoPageUrl.'"><img height="150px" id="'.$photoId.'" src="'.$photoUrl.'" /></a></td>'. "\n";
+		echo '<td><a href="'.$photoPageUrl.'" style="display: block; border: none;"><img height="150px" id="'.$photoId.'" src="'.$photoUrl.'" style="display: block; height: 150px; border: none;"/></a></td>'. "\n";
+		$codeString .= '<td><a href="'.$photoPageUrl.'" style="display: block; border: none;"><img height="150px" id="'.$photoId.'" src="'.$photoUrl.'" style="display: block; height: 150px; border: none;"/></a></td>'. "\n";
 		if($count%3 == 2){
 			?>
 		</tr>
 			<?php
+			$codeString .= "</tr>";
 		}
 		$count++;
 	}
 	?>
 	</table>
+	<?php
+	$codeString .= "</table>";
+	?>
+	<form>
+		<textarea cols="120" rows="20"><?php echo $codeString ?></textarea>
+	</form>
 	<?php
 
 }
@@ -64,7 +75,8 @@ else{
 	$apiSigHash = md5($apiSig);
 	$loginUrl = "http://flickr.com/services/auth/?api_key=".APIKEY."&perms=read&api_sig=$apiSigHash";
 
-	echo "<a href=\"$loginUrl\">Login!</a>";
+	echo "<h3>Login with:";
+	echo "<a href=\"$loginUrl\"><img height=\"30px\" src=\"Flickr-logo.png\" /></a></h3>";
 }
 
 //Create blank grid
